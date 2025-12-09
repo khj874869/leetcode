2b4649ca42ed729526db472578b3f02b7f76db05
khj874869 <134674147+khj874869@users.noreply.github.com>
2025-12-09 15:55:15 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isHappy(int n) {
+        String ns = String.valueOf(n);
+        boolean sma = false;
+        if(ns.length()>1){
+            sma = true;
+        }else{
+            
+        }
+        return sma;
+    }
+}
